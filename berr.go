eb58b31441a62b7d45952453f7f2ba249447a97f
Rheisen Dennis <rheisen.dennis@protonmail.com>
2023-01-05 23:51:48 -0500

    Add error method to Errors type

--- berr.go
+++ berr.go
@@ -1,8 +1,32 @@
 package berr
 
-import "github.com/rheisen/berr/berrconst"
+import (
+	"strings"
+
+	"github.com/rheisen/berr/berrconst"
+)
 
 type Errors []Error
+
+func (e *Errors) Error() string {
+	if e == nil {
+		return ""
+	}
+
+	builder := strings.Builder{}
+	builder.WriteString("[")
+
+	for idx, val := range *e {
+		if idx > 0 {
+			builder.WriteString(", ")
+		}
+		builder.WriteString(val.Error())
+	}
+
+	builder.WriteString("]")
+
+	return builder.String()
+}
 
 type Error interface {
 	error
